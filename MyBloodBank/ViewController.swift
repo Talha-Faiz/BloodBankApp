9b626ad193f748ab855f2de70753ca4d3997a300
Talha Faiz <imtalhafaiz@gmail.com>
2020-01-28 19:47:01 +0500

    Initial Commit

---
+++ MyBloodBank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyBloodBank
+//
+//  Created by Talha Faiz on 28/01/2020.
+//  Copyright © 2020 Talha Faiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
